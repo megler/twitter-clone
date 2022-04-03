a555cd7a8042dee886e955ec54203d2396b8be2d
megler <majeresmom@att.net>
2022-04-02 21:27:47 -0400

    added date joined to profile model.

--- project4/network/models.py
+++ project4/network/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
@@ -15,6 +16,11 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     background_image = models.URLField(null=True)
+    date_joined = models.DateField(auto_now=False,
+                                   auto_now_add=False,
+                                   null=True,
+                                   blank=True,
+                                   default=now)
 
     def __str__(self):
         return f"{self.user.first_name}"
