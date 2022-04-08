946c6edb73b3b6ea7babad36b64e375ab0e9b718
megler <majeresmom@att.net>
2022-04-08 13:04:23 -0400

    fully reworked layout.html, all-tweets works as designed, was able to remove profile variable.

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("network/all-tweets", views.all_tweets, name="all_tweets"),
     path("network/index/<int:pk>", views.user_profile, name="user_profile"),
     path("network/follow/<int:id>", views.follow, name="follow"),
     path("network/unfollow/<int:id>", views.unfollow, name="unfollow"),
--- project4/network/views.py
+++ project4/network/views.py
@@ -11,29 +11,14 @@ from .models import User, Post, Like, Profile
 
 
 def index(request):
-    """When visiting homepage, if user auth, then show user's tweets else
-    show all tweets"""
+    """When visiting homepage, show all tweets"""
 
-    profile = True
-    if request.user.is_authenticated:
-        profile = False
-        pk = request.user.id
-        tweets = Post.objects.filter(author__id=pk)
-        sorted_tweets = sort_tweets(tweets)
-        user = User.objects.get(pk=pk)
-        other_users = who_to_follow(request)[:10]
+    all_tweets = Post.objects.all()
+    tweets = sort_tweets(all_tweets)
 
-        return render(
-            request,
-            "network/index.html",
-            {
-                "profile": profile,
-                "posts": sorted_tweets,
-                "user": user,
-                "follow_suggestions": other_users,
-            },
-        )
-    return all_tweets(request)
+    return render(request, "network/index.html", {
+        "posts": tweets,
+    })
 
 
 def login_view(request):
@@ -95,22 +80,12 @@ def register(request):
         return render(request, "network/register.html")
 
 
-def all_tweets(request):
-    all_tweets = Post.objects.all()
-    tweets = sort_tweets(all_tweets)
-    profile = False
-    return render(request, "network/all-tweets.html", {
-        "posts": tweets,
-        "profile": profile
-    })
-
-
 def user_profile(request, pk):
     user_profile_id = User.objects.get(pk=pk)
     specific_tweets = Post.objects.filter(author__id=pk)
     tweets = sort_tweets(specific_tweets)
     num_tweets = tweet_count(pk)
-    profile = True
+
     # get a suggested follower list and limit to 10
     if request.user.is_authenticated:
         other_users = who_to_follow(request)[:10]
@@ -127,7 +102,6 @@ def user_profile(request, pk):
             "is_following": is_following,
             "user_follows": user_following_count,
             "user_followed_by": user_followed_by_count,
-            "profile": profile,
             "user_profile_id": user_profile_id,
             "posts": tweets,
             "follow_suggestions": other_users,
@@ -162,12 +136,9 @@ def send_tweet(request, pk):
 
 def user_following(request, pk):
     user_is_following = Profile.objects.filter(followers__id=pk)
-    profile = False
+
     return render(
         request,
         "network/index.html",
-        {
-            "profile": profile,
-            "user_is_following": user_is_following
-        },
+        {"user_is_following": user_is_following},
     )
