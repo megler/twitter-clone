1ca25d2c6ba85b0b195ce6a04126db408e4dd538
megler <majeresmom@att.net>
2022-05-19 14:39:52 -0400

    removed dev secret key. Is not used in production. Placeholder only.

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8"
+# Key found in previous git upload is placeholder only. Is not used in production.
+SECRET_KEY = "PLACEHOLDER"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
